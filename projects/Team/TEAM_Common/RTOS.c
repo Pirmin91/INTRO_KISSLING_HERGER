49196f86d5d5c4e7c8a68379855d627d60992678
tbkissli <raphael.kissling@stud.hslu.ch>
2017-04-04 11:56:20 +0200

    update due to RTOS lab (robot and common)

--- projects/Team/TEAM_Common/RTOS.c
+++ projects/Team/TEAM_Common/RTOS.c
@@ -8,13 +8,36 @@
 #if PL_CONFIG_HAS_RTOS
 #include "RTOS.h"
 #include "FRTOS1.h"
+/* User includes (#include below this line is not maintained by Processor Expert) */
+#include "LED.h"
 
 void RTOS_Init(void) {
   /*! \todo Create tasks here */
+	//Create main task
+	BaseType_t res;
+	xTaskHandle taskHndl ;
+	res = xTaskCreate(MainTask,"Main",configMINIMAL_STACK_SIZE+50 /*1kByte Stack*/,
+		(void*)NULL,tskIDLE_PRIORITY+1 /*um eins höhere Prio als idle task*/,&taskHndl);
+
+	//error handling /*! \todo do Error Handling here */
+	//if ( res!=pdPASS) {
+	//}
+
 }
 
 void RTOS_Deinit(void) {
   /* nothing needed for now */
+}
+
+static void MainTask(void *pvParameters) {
+	for(;;) {
+		//Blinking an LED
+		LEDPin1_NegVal();
+#if PL_LOCAL_CONFIG_BOARD_IS_ROBO
+		LEDPin2_NegVal();
+#endif
+		vTaskDelay(200/portTICK_PERIOD_MS);		//200ms Blinkperiode
+	}
 }
 
 #endif /* PL_CONFIG_HAS_RTOS */
