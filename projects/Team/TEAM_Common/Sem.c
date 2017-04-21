92afee0e5be2a8d71d354ba562d8c7334df2e65b
Pirmin91 <pirmin.herger@stud.hslu.ch>
2017-04-21 15:29:49 +0200

    lab 24 (semaphore), implementation sem.c and local.h

--- projects/Team/TEAM_Common/Sem.c
+++ projects/Team/TEAM_Common/Sem.c
@@ -20,26 +20,56 @@
 #include "Sem.h"
 #include "LED.h"
 
+static SemaphoreHandle_t xSemaphore;
+
 static void vSlaveTask(void *pvParameters) {
+	while(xSemaphore == NULL)
+	{
+		vTaskDelay(10);	// wait 10ms
+	}
    for(;;) {
     /*! \todo Implement functionality */
-  }
+	     if (xSemaphoreTake(xSemaphore, portMAX_DELAY)==pdPASS) { /* block on semaphore */
+	       LED1_Neg();
+	     }
+   }
 }
 
 static void vMasterTask(void *pvParameters) {
   /*! \todo send semaphore from master task to slave task */
+	while(xSemaphore == NULL)
+	{
+		vTaskDelay(10);	// wait 10ms
+	}
   for(;;) {
     /*! \todo Implement functionality */
+	    (void)xSemaphoreGive(xSemaphore); /* give control to other task */
+	    vTaskDelay(1000/portTICK_PERIOD_MS);
   }
 }
+
 
 void SEM_Deinit(void) {
 }
 
 /*! \brief Initializes module */
-void SEM_Init(void) {
-  if (xTaskCreate(vMasterTask, "Master", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY+1, NULL) != pdPASS) {
-    for(;;){} /* error */
-  }
+void SEM_Init(void)
+{
+	xSemaphore = xSemaphoreCreateBinary(); // Creating Binary Semaphore
+	if (xSemaphore == NULL)
+	{
+		// Failed! not enough heap memory! FOLIE 90
+	}
+	else {
+			// Master -> function above
+			if (xTaskCreate(vMasterTask, "Master", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY+1, NULL) != pdPASS) {
+				for(;;){} /* error */
+			}
+
+			// Slave -> function above
+			if (xTaskCreate(vMasterTask, "Slave", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY+1, NULL) != pdPASS) {
+				for(;;){} /* error */
+			}
+	}
 }
 #endif /* PL_CONFIG_HAS_SEMAPHORE */
